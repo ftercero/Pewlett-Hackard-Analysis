2458c570d4917cc4a2bcf1f79b1de06c48a3f380
Fidel Tercero <hueso@Fidels-MBP.attlocal.net>
2022-01-08 21:52:09 -0800

    Export new files and updated queries

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,3 +1,23 @@
+-- Deliverable 1
+-- determine the number of retiring employees per title and
+-- identify employees who are eligible to participate in a mentorship program.
+
+SELECT e.emp_no,
+	   e.first_name,
+	   e.last_name,
+	   ti.title,
+	   ti.from_date,
+	   ti.to_date
+INTO retirement_tables
+FROM employees AS e
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no;
+
+SELECT *
+FROM retirement_tables;
+
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
 rt.first_name,
@@ -18,3 +38,32 @@ INTO retiring_titles
 FROM unique_titles AS ut
 GROUP BY (ut.title)
 ORDER BY count DESC;
+
+-- Create a mentorship eligibility table.
+-- Select columns
+SELECT DISTINCT ON (e.emp_no)
+ 	e.emp_no,
+ 	e.first_name,
+    e.last_name,
+ 	e.birth_date,
+ 	de.from_date,
+ 	de.to_date,
+ 	tl.title
+-- Create new table
+INTO mentorship_eligibility
+-- Define alias and tables to join
+FROM
+	employees AS e
+INNER JOIN
+	dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN
+	titles AS tl
+ON (e.emp_no = tl.emp_no)
+-- Filter by current status and retirement age
+WHERE de.to_date = ('9999-01-01') AND
+	  (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+-- Order by employee number
+ORDER BY emp_no;
+
+Deliverable 2
